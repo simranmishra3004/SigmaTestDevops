b1c794edea74814db6bc3fc59c096c6340e2327c
Simran Mishra <simran.mishra@hansencx.com>
2022-07-27 16:43:36 +0530

    Added simran in java file

--- src/test/java/HelloWorldJunit.java
+++ src/test/java/HelloWorldJunit.java
@@ -8,7 +8,7 @@ public class HelloWorldJunit
 	public void helloWorldTest() 
 	{
 		HelloWorld hw =new HelloWorld();
-		String name=hw.isName("Yash");
+		String name=hw.isName("Simran");
 		System.out.println("Hello World "+name+" !!");
 	}
 	
