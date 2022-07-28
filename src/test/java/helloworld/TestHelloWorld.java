175161e7e7fb1169c41e29bfb8768ddeae60274a
Simran Mishra <simran.mishra@hansencx.com>
2022-07-28 10:37:28 +0530

    added some changes

---
+++ src/test/java/helloworld/TestHelloWorld.java
@@ -0,0 +1,17 @@
+import helloworld.HelloWorld;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestHelloWorld
+{
+	
+	@Test
+	public void helloWorldTest() 
+	{
+		HelloWorld hw =new HelloWorld();
+		String name=hw.isName("Simran");
+		System.out.println("Hello World "+name+" !!");
+		assertSame("Simran", name);
+	}
+	
+}
